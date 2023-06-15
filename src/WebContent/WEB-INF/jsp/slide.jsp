07d55f0b9af776b8c813357bbf19164b1581a961
dojo2023-oyanagi <130334618+dojo2023-oyanagi@users.noreply.github.com>
2023-06-15 13:45:38 +0900

    一旦

--- src/WebContent/WEB-INF/jsp/slide.jsp
+++ src/WebContent/WEB-INF/jsp/slide.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +9,18 @@
 <link rel="stylesheet" type="text/css" href="/sobaudon/css/title.css">
 </head>
 <body>
+<a href="/sobaudon/RegistrationServlet" style="padding-bottom:0;">
+  <img src="/img/logo.png" style="width:100px" />
+</a>
+
+<div>写真の日付</div>
+<div class="slider">
+        <div class="slider-inner">
+            <div class="slider-item"><img src="test1.png"></div>
+            <div class="slider-item"><img src="test2.png"></div>
+            <div class="slider-item"><img src="test3.png"></div>
+        </div>
+    </div>
 
 </body>
 </html>
--- src/src/servlet/SlideServlet.java
+++ src/src/servlet/SlideServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SlideServlet
@@ -22,11 +21,11 @@ public class SlideServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/sobaudon/LoginServlet");
-			return;
-		}
+		//HttpSession session = request.getSession();
+		//if (session.getAttribute("id") == null) {
+			//response.sendRedirect("/sobaudon/LoginServlet");
+			//return;
+		//}
 
 		// スライドショーページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/slide.jsp");
@@ -38,11 +37,41 @@ public class SlideServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/sobaudon/LoginServlet");
-			return;
-		}
+		//HttpSession session = request.getSession();
+		//if (session.getAttribute("id") == null) {
+			//response.sendRedirect("/sobaudon/LoginServlet");
+			//return;
+		//}
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String number = request.getParameter("NUMBER");
+				String company = request.getParameter("COMPANY");
+				String department = request.getParameter("DEPARTMENT");
+				String position = request.getParameter("POSITION");
+				String name = request.getParameter("NAME");
+				String zipcode = request.getParameter("ZIPCODE");
+				String address = request.getParameter("ADDRESS");
+				String phone = request.getParameter("PHONE");
+				String fax = request.getParameter("FAX");
+				String email = request.getParameter("EMAIL");
+				String remarks = request.getParameter("REMARKS");
+
+				// 検索処理を行う
+				//BcDAO bDao = new BcDAO();
+				//List<Bc> cardList = bDao.select(new Bc(number, company, department, position, name, zipcode, address, phone, fax, email, remarks));
+
+
+
+
+
+				// 検索結果をリクエストスコープに格納する
+				//request.setAttribute("cardList", cardList);
+
+
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search_result.jsp");
+				dispatcher.forward(request, response);
 	}
 
 }
