0d7201dc5537a5d5536c103a5dc42b54f29e8fe6
dojo2023-oyanagi <130334618+dojo2023-oyanagi@users.noreply.github.com>
2023-06-15 11:35:07 +0900

    一旦

--- src/WebContent/WEB-INF/jsp/slide.jsp
+++ src/WebContent/WEB-INF/jsp/slide.jsp
@@ -1,10 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>スライドショー画面</title>
+<link rel="stylesheet" type="text/css" href="/sobaudon/css/slide.css">
+<link rel="stylesheet" type="text/css" href="/sobaudon/css/header.css">
+<link rel="stylesheet" type="text/css" href="/sobaudon/css/title.css">
 </head>
 <body>
 
--- src/src/servlet/SlideServlet.java
+++ src/src/servlet/SlideServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SlideServlet
@@ -13,29 +16,29 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SlideServlet")
 public class SlideServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SlideServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/sobaudon/LoginServlet");
+			return;
+		}
+
+		// スライドショーページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/slide.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	//protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//	// TODO Auto-generated method stub
+	//	doGet(request, response);
+	//}
 
 }
