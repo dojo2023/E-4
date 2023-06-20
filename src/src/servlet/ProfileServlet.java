767d906b1192ca75004f9c12564992f5a2c1f349
dojo2023-katsuamata <katsumatahiroplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-20 17:37:05 +0900

    20

--- src/src/servlet/ProfileServlet.java
+++ src/src/servlet/ProfileServlet.java
@@ -25,15 +25,11 @@ public class ProfileServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		String user_id = (String)session.getAttribute("loginUser.user_id");
-		String password = (String)session.getAttribute("loginUser.password");
-		String name = (String)session.getAttribute("loginUser.name");
-		Double height = (Double)session.getAttribute("loginUser.height");
-		Double weight = (Double)session.getAttribute("loginUser.weight");
-		Double target_weight = (Double)session.getAttribute("loginUser.target_weight");
+		User user_inf = (User)session.getAttribute("loginUser");
+
 		UserDao uDao = new UserDao();
 		//検索処理を行う
-			User profile = uDao.detail(new model.User(user_id, password, name, height, weight, target_weight));
+			User profile = uDao.detail(user_inf);
 		//更新内容をリクエストスコープに格納する
 		request.setAttribute("profile", profile);
 		// プロフィール編集画面にフォワードする
