b5be3c11cf1aad639ef4f20a02f43b4371483b92
dojo2023-nishimura <130331154+dojo2023-nishimura@users.noreply.github.com>
2023-06-22 09:33:16 +0900

    a

--- src/src/test/UserDaoTest.java
+++ src/src/test/UserDaoTest.java
@@ -18,36 +18,22 @@ public class UserDaoTest {
 			System.out.println("NAME" + user.getWeight());
 			System.out.println("ZIPCODE" + user.getTarget_weight());
 			System.out.println();
-	}
-}
-/*
+
+
 		// insert()のテスト
 		System.out.println("---------- insert()のテスト ----------");
-		Bc insRec = new Bc(" ","会社名６", "部署名６", "役職名６",
-				"氏名６", "666-6666", "住所６", "6666-66-6666", "6666-66-0606",
-				"mail6@company6.jp", "備考６");
+		User insRec = new User("dojo", "password", "" , null, null, null);
 		if (dao.insert(insRec)) {
 			System.out.println("登録成功！");
-			List<Bc> cardList3 = dao.select(insRec);
-			for (Bc card : cardList3) {
-				System.out.println("NUMBER：" + card.getNumber());
-				System.out.println("COMPANY" + card.getCompany());
-				System.out.println("DEPARTMENT" + card.getDepartment());
-				System.out.println("POSITION" + card.getPosition());
-				System.out.println("NAME" + card.getName());
-				System.out.println("ZIPCODE" + card.getZipCode());
-				System.out.println("ADDRESS：" + card.getAddress());
-				System.out.println("PHONE" + card.getPhone());
-				System.out.println("FAX" + card.getFax());
-				System.out.println("MAIL" + card.getMail());
-				System.out.println("REMARKS" + card.getRemarks());
-				System.out.println();
-			}
 		}
 		else {
 			System.out.println("登録失敗！");
 		}
+	}
+}
 
+
+/*
 		// update()のテスト
 		System.out.println("---------- update()のテスト ----------");
 		Bc upRec = new Bc();
