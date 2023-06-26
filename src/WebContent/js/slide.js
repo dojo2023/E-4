9846b2ac88f4017153ab32da20ffc6ade9d98d3b
dojo2023-oyanagi <130334618+dojo2023-oyanagi@users.noreply.github.com>
2023-06-26 10:10:23 +0900

    a

--- src/WebContent/js/slide.js
+++ src/WebContent/js/slide.js
@@ -1,33 +1,64 @@
-$(function() {
- $('.slider').each(function() {
-   // スライド（画像）の数を取得
-   var $slides = $(this).find('img'),
-       slideNum = $slides.length,
-       currentIdx = 0; // 何番目か
 
-   // 最初の画像をフェードイン
-   $(".slider img").eq(currentIdx).fadeIn();
+// class名"a"の要素を取得
+const elements = document.getElementsByClassName('slide img');
+const elementsd = document.getElementsByClassName('b');
 
-   // 3秒後に次のスライドを表示
-   setTimeout(dispNextSlide, 3000);
+let paused = true;  // 開始、停止用
+let num = -1;        // 配列の要素番号用
+let n = 1;          // setIntervalのID用
+let speed = 1000;   // スライドショーの速度(ミリ秒)
+let elem = document.getElementById('select_box');
 
-   // 次のスライドを表示するメソッド
-   function dispNextSlide() {
-     var nextIdx = currentIdx + 1;
 
-     // 最後のスライドの場合ははじめに戻る
-     if (nextIdx > (slideNum - 1)) {
-       nextIdx = 0
-     }
+// slideshow_timerメソッド
+function slideshow_timer(){
+  if (num === elements.length - 1){
+    num = 0;
+  }
+  else {
+    num ++;
+  }
+  document.getElementById("mypic").src = elements[num].value;
+  document.getElementById("date").innerHTML = elementsd[num].value;
+}
 
-     // 現在のスライドをフェードアウト
-     $(".slider img").eq(currentIdx).fadeOut();
+// 初期のスライドショーの起動
+let timer = setInterval(function(){
+  if (paused) {
+    console.log("一時停止");
+    return;
+  }
 
-     // 次のスライドをフェードイン
-     $(".slider img").eq(nextIdx).fadeIn();
+  slideshow_timer()}, speed);
 
-     // インデックスを更新
-     currentIdx = nextIdx;
-   }
- });
+// 停止ボタンを押したとき
+document.getElementById('btnPause').onclick = function(){
+    paused = true;
+}
+
+// 開始ボタンを押したとき
+document.getElementById('btnResume').onclick = function(){
+  paused = false;
+}
+
+// リセットボタンを押したとき
+document.getElementById('reset').onclick
+= function(){
+  clearInterval(n);           // setIntervalのIDを入れることでsetIntervalを削除
+  n++;                        //console.log(n);
+  num = elements.length - 1;  // 配列の先頭の要素を取り出す
+  paused = true;              // 停止状態にする
+  document.getElementById("mypic").src = null; //初期値の画像を挿入
+  document.getElementById("date").innerHTML = "日付"; //初期値の日付テキストを挿入
+
+  timer = setInterval(function(){ // 再度setIntervalを呼び出す
+    if (paused) {
+      console.log("一時停止");
+      return
+    }
+    slideshow_timer()}, speed);
+}
+// 速度変更
+elem.addEventListener('change', function(){
+  speed = elem.value; // console.log(speed);
 });
