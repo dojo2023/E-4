23053bb89bdc1b46974eb82c27e41930c8e825ac
dojo2023-nishimura <130331154+dojo2023-nishimura@users.noreply.github.com>
2023-06-26 16:25:34 +0900

    Update BrowseServlet.java

--- src/src/servlet/BrowseServlet.java
+++ src/src/servlet/BrowseServlet.java
@@ -29,7 +29,7 @@ public class BrowseServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
+		if (session.getAttribute("profile") == null) {
 			response.sendRedirect("/sobaudon/LoginServlet");
 			return;
 		}
