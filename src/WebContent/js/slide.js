927af97cb63fd6f43f0ad4bc933069fa1f6f6731
dojo2023-oyanagi <130334618+dojo2023-oyanagi@users.noreply.github.com>
2023-06-27 11:47:24 +0900

    a

--- src/WebContent/js/slide.js
+++ src/WebContent/js/slide.js
@@ -48,8 +48,8 @@ document.getElementById('reset').onclick
   n++;                        //console.log(n);
   num = elements.length - 1;  // 配列の先頭の要素を取り出す
   paused = true;              // 停止状態にする
-  document.getElementById("mypic").src = null; //初期値の画像を挿入
-  document.getElementById("date").innerHTML = "日付"; //初期値の日付テキストを挿入
+  document.getElementById("mypic").src = elements[0].value; //初期値の画像を挿入
+  document.getElementById("date").innerHTML = elementsd[0].value; //初期値の日付テキストを挿入
 
   timer = setInterval(function(){ // 再度setIntervalを呼び出す
     if (paused) {
